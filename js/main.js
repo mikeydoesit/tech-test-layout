d9014c66b79a56b40421e8634cf1018f8fe545a8
mikeydoesit <michaelldninc@gmail.com>
2019-03-10 23:10:01 +0000

    if statement for see more/less button text

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
+
   $('#toggleIcons').on('click', function() {
-      $('#secondRowOfIcons').slideToggle(500);
+
+      $('#secondRowOfIcons').slideToggle(500, function() {
+        $('#toggleIcons').text() == 'See More' ? $('#toggleIcons').text('See Less') : $('#toggleIcons').text('See More')
+      });
   });
 });
